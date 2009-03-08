2716fa3ab63affe422fb454df4a43eebb8f8c93d
R. Tyler Ballance <tyler@monkeypox.org>
2009-03-08 06:28:14 -0400

    Add the FastJS_Write() function to provide a means for the JavaScript executing on the server side to access the output stream
    We also have jQuery provided in the global scope for the executing JavaScript
    
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

--- fastjs.cc
+++ fastjs.cc
@@ -26,6 +26,11 @@ using namespace v8; // *puke*
 #define JQUERY_FILE "jquery-1.3.2.min.js"
 #define JQUERY_COMPAT "if (typeof(window) == 'undefined'){window=new Object();document=window;self=window;navigator=new Object();navigator.userAgent=navigator.userAgent||'FastJS Server';location=new Object();location.href='file:///dev/null';location.protocol='file:';location.host = 'FastJS';};"
 
+/* Functions that will be exposed into the JavaScript environment */
+Handle<Value> FastJS_Write(const Arguments& args);
+Handle<Value> FastJS_Load(const Arguments& args);
+
+FCGX_Stream *_current_stream = NULL; /* Unfortunately we need a global to access the stream inside JavaScript callbacks */
 
 static void PrintEnv(FCGX_Stream *out, char *label, char **envp)
 {
@@ -61,32 +66,42 @@ static void *load_jquery()
 	return jquery;
 }
 
+
 static void req_handle(FCGX_Stream *out, char **environment) 
 {
-	HandleScope scope;
-	Persistent<Context> ctx = Context::New();
-	Context::Scope ctx_scope(ctx);
-
-	Handle<String> source = String::New("(function() { return 'OMG'; })()");
-	Handle<Script> script = Script::Compile(source);
-
-	Handle<Value> rc = script->Run();
-	ctx.Dispose();
-
-	String::AsciiValue rc_str(rc);
-
-	FCGX_FPrintF(out, "Our test() JavaScript function returned:<br/>\n");
-	FCGX_FPrintF(out, *rc_str);
-	FCGX_FPrintF(out, "<br/><br/><br/>");
-
-	PrintEnv(out, "FastCGI Environment:", environment);
-
 	void *jq = load_jquery();
 	if (jq == NULL) {
 		/* FAIL! */
 		FCGX_FPrintF(out, "FAILED TO PROPERLY LOAD JQUERY!");
 		return;
 	}
+
+	HandleScope scope;
+	Handle<ObjectTemplate> _global = ObjectTemplate::New();
+	_global->Set(String::New("write"), FunctionTemplate::New(FastJS_Write));
+	Persistent<Context> ctx = Context::New(NULL, _global);
+	Context::Scope ctx_scope(ctx);
+
+	Handle<String> prereq = String::New(JQUERY_COMPAT);
+	Handle<Script> prereqs = Script::Compile(prereq);
+	prereqs->Run();
+
+	Handle<String> jquery = String::New((const char *)(jq));
+	Handle<Script> jqscript = Script::Compile(jquery);
+	jqscript->Run();
+
+	Handle<String> source = String::New("(function() { write('Looping over location:'); $.each(location, function(index) { write(this); }); })();");
+	Handle<Script> script = Script::Compile(source);
+	
+	Handle<Value> rc = script->Run();
+	ctx.Dispose();
+	/*
+	String::AsciiValue rc_str(rc);
+	FCGX_FPrintF(out, *rc_str);
+	*/
+
+	PrintEnv(out, "<br/><br/>FastCGI Environment:", environment);
+
 }
 
 int main ()
@@ -96,6 +111,7 @@ int main ()
     int count = 0;
 
     while (FCGX_Accept(&in, &out, &err, &envp) >= 0) {
+		_current_stream = out;
         char *contentLength = FCGX_GetParam("CONTENT_LENGTH", envp);
         int len = 0;
 
@@ -110,7 +126,18 @@ int main ()
             len = strtol(contentLength, NULL, 10);
 	
 		req_handle(out, envp);
+		_current_stream = NULL;
     } /* while */
 
     return 0;
+}
+
+Handle<Value> FastJS_Write(const Arguments& args)
+{
+	for (int i = 0; i < args.Length(); ++i) {
+		String::Utf8Value str(args[i]);
+		FCGX_FPrintF(_current_stream, *str);
+	}
+	FCGX_FPrintF(_current_stream, "\n");
+	return Undefined();
 }
