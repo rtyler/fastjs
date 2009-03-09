0f50b14e6dc510e7e5e551ebaf1f3190ba5002c5
R. Tyler Ballance <tyler@monkeypox.org>
2009-03-09 03:12:02 -0400

    Reading in .fjs files not and executing them accordingly. Running index.fjs as a good test page.
    Still need to get the load function and the environment variables exposed into the JavaScript through a global object under window.fastjs
    
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

--- fastjs.cc
+++ fastjs.cc
@@ -15,6 +15,7 @@ extern "C" {
 	extern char **environ;
 	#include <fcgi_config.h>
 	#include <fcgiapp.h>
+	#include <strings.h>
 	#include <sys/types.h>
 	#include <sys/stat.h>
 	#include <fcntl.h>
@@ -24,13 +25,16 @@ extern "C" {
 using namespace v8; // *puke*
 
 #define JQUERY_FILE "jquery-1.3.2.min.js"
-#define JQUERY_COMPAT "if (typeof(window) == 'undefined'){window=new Object();document=window;self=window;navigator=new Object();navigator.userAgent=navigator.userAgent||'FastJS Server';location=new Object();location.href='file:///dev/null';location.protocol='file:';location.host = 'FastJS';};"
+#define JQUERY_COMPAT "if (typeof(window) == 'undefined'){window=new Object();document=window;self=window;navigator=new Object();navigator.userAgent=navigator.userAgent||'FastJS Server';location=new Object();location.href='file:///dev/null';location.protocol='file:';location.host = 'FastJS';}"
 
 /* Functions that will be exposed into the JavaScript environment */
 Handle<Value> FastJS_Write(const Arguments& args);
 Handle<Value> FastJS_Load(const Arguments& args);
 
-FCGX_Stream *_current_stream = NULL; /* Unfortunately we need a global to access the stream inside JavaScript callbacks */
+/* Unfortunately we need a few global pointers to our current streams for the JavaScript callbacks */
+FCGX_Stream *_in_stream = NULL;
+FCGX_Stream *_out_stream = NULL; 
+FCGX_Stream *_error_stream = NULL;
 void *_jQuery = NULL; /* Global jQuery buffer to prevent needing to re-read the file per-request */
 
 static void PrintEnv(FCGX_Stream *out, char *label, char **envp)
@@ -54,7 +58,8 @@ static void *read_file_contents(const char *filepath)
 		return NULL;
 	}
 	
-	void *buffer = malloc(sizeof(char) * (attributes.st_size + 2));
+	void *buffer = malloc(sizeof(char) * (attributes.st_size + 1));
+	bzero(buffer, (attributes.st_size + 1));
 	fd = open(filepath, 0);
 	if (fd == 0) {
 		/* I should do something with the error here */
@@ -62,7 +67,7 @@ static void *read_file_contents(const char *filepath)
 		return NULL;
 	}
 
-	rc= read(fd, buffer, attributes.st_size);
+	rc = read(fd, buffer, attributes.st_size);
 	close(fd);
 	return buffer;
 }	
@@ -90,18 +95,25 @@ static void req_handle(FCGX_Stream *out, char **environment)
 	Handle<Script> jqscript = Script::Compile(jquery);
 	jqscript->Run();
 
-	Handle<String> source = String::New("(function() { write('Looping over location:'); $.each(location, function(index) { write(this); }); })();");
+	void *index = read_file_contents("pages/index.fjs");
+	Handle<String> source = String::New( (const char *)(index) );
+	TryCatch ex;
 	Handle<Script> script = Script::Compile(source);
-	
+
+	if (script.IsEmpty()) {
+		Handle<Value> exception = ex.Exception();
+		String::AsciiValue ex_str(exception);
+		fprintf(stderr, *ex_str);
+		fprintf(stderr, "\n\n");
+	}
+
 	Handle<Value> rc = script->Run();
 	ctx.Dispose();
+
 	/*
 	String::AsciiValue rc_str(rc);
-	FCGX_FPrintF(out, *rc_str);
+	fprintf(stderr, *rc_str);
 	*/
-
-	PrintEnv(out, "<br/><br/>FastCGI Environment:", environment);
-
 }
 
 int main ()
@@ -113,22 +125,25 @@ int main ()
 	_jQuery = read_file_contents(JQUERY_FILE);
 
     while (FCGX_Accept(&in, &out, &err, &envp) >= 0) {
-		_current_stream = out;
+		_in_stream = in;
+		_error_stream = err;
+		_out_stream = out;
         char *contentLength = FCGX_GetParam("CONTENT_LENGTH", envp);
         int len = 0;
 
         FCGX_FPrintF(out,
            "Content-type: text/html\r\n"
            "\r\n"
-           "<html><title>FastJS Sample Page</title>"
-           "<h1>FastJS</h1>\n"
-           "Request number %d,  Process ID: %d<p>\n", ++count, getpid());
+		   );
 
         if (contentLength != NULL)
             len = strtol(contentLength, NULL, 10);
 	
 		req_handle(out, envp);
-		_current_stream = NULL;
+
+		_error_stream = NULL;
+		_in_stream = NULL;
+		_out_stream = NULL;
     } /* while */
 
 	if (_jQuery)
@@ -140,9 +155,9 @@ Handle<Value> FastJS_Write(const Arguments& args)
 {
 	for (int i = 0; i < args.Length(); ++i) {
 		String::Utf8Value str(args[i]);
-		FCGX_FPrintF(_current_stream, *str);
+		FCGX_FPrintF(_out_stream, *str);
 	}
-	FCGX_FPrintF(_current_stream, "\n");
+	FCGX_FPrintF(_out_stream, "\n");
 	return Undefined();
 }
 /*
